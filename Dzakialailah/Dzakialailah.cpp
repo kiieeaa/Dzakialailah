09b8ed31057bf2b87a0177e8cec3ed2cadeb5752
LENOVO <LENOVO@LAPTOP-AI2PIVR2>
2024-02-26 20:51:04 +0700

    Add project files.

---
+++ Dzakialailah/Dzakialailah.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //begin
+    //numeric nJejari, nLuas
+    //display 'masukan jejari='
+    //accept nJejari
+    //compute nLuas = 3.14 * r * r
+//end
+
+    double nJejari, nLuas;
+    cout << "masukan jejari = ";
+    cin >> nJejari;
+    nluas = 3.14 * nJeja
+}
+
